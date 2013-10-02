85be6b4aecb477ded17f4e28b7317ee05f9b67d1
Ricky Laboe <rickylaboezerozero@yahoo.com>
2013-10-02 11:38:05 -0700

    created and added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bear::Application.config.secret_token = 'c5813e6501e7df04479644c71261b9164ac57e7fbd332d6c64745415f21c1e79ccb3140c994e141da66a4630ec47a2f9a583681eb8e84eca4370cb485aaf7c25'
